b97786340aeaaeeb8087c4850730aa1d35746fb2
chaslinux <chaslinux@gmail.com>
2026-01-14 09:32:06 -0500

    Updates for newer kernels, this should fix at least the Surface 7+ working on Ubuntu 24.04

--- surface-7plus.sh
+++ surface-7plus.sh
@@ -5,6 +5,8 @@
 
 # this script updates the Ubuntu system and installs the touch drivers for the
 # Microsoft Surface Pro 7+
+# This borrows from a couple of places, but most recently:
+# https://github.com/linux-surface/linux-surface/wiki/Installation-and-Setup
 
 # This surface directory should be the REPODIR
 REPODIR=$(pwd)
@@ -37,3 +39,24 @@ fi
 # enable service
 sudo systemctl enable iptsd
 sudo systemctl start iptsd
+
+# Import the linux-surface driver and trust the key
+wget -qO - https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc \
+    | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/linux-surface.gpg
+
+# Add the repo to apt and update
+echo "deb [arch=amd64] https://pkg.surfacelinux.com/debian release main" \
+	| sudo tee /etc/apt/sources.list.d/linux-surface.list
+sudo apt update
+
+# Install the surface kernel driver and dependencies
+sudo apt install linux-image-surface linux-headers-surface libwacom-surface iptsd -y
+
+# Set up a secureboot mok key, the password should be surface
+sudo apt install linux-surface-secureboot-mok -y
+
+# update grub
+sudo update-grub
+
+echo "when prompted for a MOK key, enroll the MOK key with the password shown earlier, it should be surface."
+
